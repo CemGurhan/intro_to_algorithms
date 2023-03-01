a42b005ad32f12f3d08d207b63cc02f97bb8bbff
CemGurhan <cemg@live.co.uk>
2023-03-01 08:57:16 +0000

    gitignored target in all directories and removed target from current project

--- target/debug/deps/insertion_sort-5cfced0f303dd68b.d
+++
@@ -1,5 +0,0 @@
-/Users/cemgurhan/intro_to_algorithms/target/debug/deps/insertion_sort-5cfced0f303dd68b.rmeta: insertion_sort/src/main.rs
-
-/Users/cemgurhan/intro_to_algorithms/target/debug/deps/insertion_sort-5cfced0f303dd68b.d: insertion_sort/src/main.rs
-
-insertion_sort/src/main.rs:
