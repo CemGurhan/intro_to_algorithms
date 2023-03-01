1e9f8369ce35b2129c3ff5ffd53dd33511bf8f94
CemGurhan <cemg@live.co.uk>
2023-03-01 09:47:54 +0000

    added complete insertion sort

--- insertion_sort/src/main.rs
+++ insertion_sort/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut array = [4,1,5,3,7,9];
+    insertion_sort(&mut array)
+}
+
+// insertion sort sorts an array in ascending order
+fn insertion_sort(array: &mut [i32]) {
+    for j in 1..array.len() {
+        let mut key = array[j];
+        let mut i = j - 1;
+
+        while i > 0 && array[i] > key {
+            array[i + 1] = array[i];
+            i = i - 1;
+        }
+        array[i] = key;
+    }
+    println!("Sorted array: {:?}", array);
 }
