0d83e5f3ac94130701fe7fcb18765ba6a32b245c
CemGurhan <cemg@live.co.uk>
2023-03-02 08:57:22 +0000

    added non-increasing insertion sort

--- insertion_sort/src/main.rs
+++ insertion_sort/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     let mut array2 = [2,3,4,1,0,-3,10];
     insertion_sort(&mut array1);
     insertion_sort(&mut array2);
+    non_increasing_insertion_sort(&mut array1);
+    non_increasing_insertion_sort(&mut array2);
 }
 
 // insertion sort sorts an array in ascending order
@@ -15,6 +17,17 @@ fn insertion_sort(array: &mut [i32]) {
         }
     }
     println!("sorted array: {:?}", array);
+}
+
+fn non_increasing_insertion_sort(array: &mut [i32]) {
+    for i in 1..array.len() {
+        for j in 0..i {
+            if array[j] < array[i] {
+                array.swap(i, j);
+            }
+        }
+    }
+    println!("non-increasing sorted array: {:?}", array);
 }
 
 
